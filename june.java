96406533d703e10654c2c9f7646b78b0c292cb35
Dharanipilla <luxelovely18@gmail.com>
2024-06-24 18:48:08 +0530

    Update june.java

--- june.java
+++ june.java
@@ -1,6 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("hello,master!");
+        System.out.println("hello,master!")
+            thanks masteru;
     }
 }
